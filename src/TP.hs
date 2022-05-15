0665bf7c17d1c2e27850d8a076d4f182f49056a4
ErnestinaKerbs <ekerbs@frba.utn.edu.ar>
2022-05-15 18:35:56 -0300

    Resolucion partes 1,2 y 3

--- src/TP.hs
+++ src/TP.hs
@@ -14,6 +14,94 @@ afectarALosQueCumplen :: (a -> Bool) -> (a -> a) -> [a] -> [a]
 afectarALosQueCumplen criterio efecto lista
   = (map efecto . filter criterio) lista ++ filter (not.criterio) lista
 
+--me devuelve una lista con TODOS LOS ELEMENTOS INICIALES PERO CON EFECTOS APLICADOS SOLO A LOS QUE CUMPLEN EL CRITERIO
 ----------------------------------------------------------------------
 -- Definir los tipos de datos y funciones para el TP a partir de acá
 ----------------------------------------------------------------------
+
+data Auto =  Auto{
+
+  color::String,
+  velocidad::Int,
+  distancia::Int
+
+}deriving (Show,Eq)
+
+data Carrera = Carrera{
+
+  autos::[Auto]
+
+}deriving (Show,Eq)
+
+{--------------------PARTE 1--------------------}
+
+estaCerca::Auto->Auto->Bool
+estaCerca auto1 auto2 = (distintoDe auto1 auto2) && (distanciaEntre auto1 auto2 < 10)
+
+--extra 1
+distintoDe::Eq a => a->a->Bool
+distintoDe elemento1 = not.(elemento1 ==)
+
+distanciaEntre::Auto->Auto->Int
+distanciaEntre auto1 = abs.(distancia auto1-).distancia
+---------
+
+vaTranquilo::Auto->Carrera->Bool
+vaTranquilo elAuto carrera = (all (estaCerca elAuto) (autos carrera)) && (all (vaGanando elAuto) (autos carrera))
+
+--extra 2
+vaGanando::Auto->Auto->Bool
+vaGanando auto1 = (distancia auto1>).distancia
+---------
+
+puesto::Auto->Carrera->Int
+puesto elAuto carrera = 1 + length (autos carrera) - length (filter (vaGanando elAuto) (autos carrera))
+{-puesto elAuto carrera = 1 + length (filter not.(vaGanando elAuto) (autos carrera))
+no sirve-}
+{-SOLUCION: cantidad total de autos - cantidad de autoas a los elAuto les va ganando-}
+
+{-----------------------------------------------}
+
+{--------------------PARTE 2--------------------}
+
+correrDurante::Int->Auto->Auto
+correrDurante tiempo unAuto = Auto (color unAuto) (velocidad unAuto) (distancia unAuto + tiempo*(velocidad unAuto)) 
+
+alterarVelocidad::(Int->Int->Int)->Int->Auto->Auto
+alterarVelocidad funcion cantidad unAuto = Auto (color unAuto) (funcion (velocidad unAuto) cantidad) (distancia unAuto)
+
+bajarVelocidad::Int->Int->Int
+bajarVelocidad velocidadActual cantidad 
+  |velocidadActual>cantidad = velocidadActual - cantidad
+  |otherwise = 0
+{-----------------------------------------------}
+
+{--------------------PARTE 3--------------------}
+
+type PowerUp = Auto->Carrera->[Auto]
+
+aplicarPowerUp::PowerUp->Auto->Carrera->Carrera
+aplicarPowerUp funcionPowerUp autoTrigger carrera = Carrera (funcionPowerUp autoTrigger carrera)
+
+terremoto::PowerUp
+terremoto autoTrigger = afectarALosQueCumplen (estaCerca autoTrigger) (alterarVelocidad (bajarVelocidad) 50).autos
+
+miguelitos::Int->PowerUp
+miguelitos cantidadABajar autoTrigger = (afectarALosQueCumplen (vaGanando autoTrigger)
+                                         (alterarVelocidad bajarVelocidad cantidadABajar)).autos 
+
+jetpack::Tiempo->PowerUp
+jetpack tiempo autoTrigger carrera = afectarALosQueCumplen (==autoTrigger) 
+                                    (alterarDistancia (bonusJetpack autoTrigger (2*(velocidad autoTrigger)) tiempo))
+                                    (autos carrera)
+--extra 3                                 
+type Velocidad=Int
+type Tiempo = Int
+type Distancia = Int
+bonusJetpack::Auto->Velocidad->Tiempo->Distancia
+bonusJetpack autoAfectado aumentoVelocidad tiempo = ((velocidad autoAfectado)+aumentoVelocidad)*tiempo
+
+alterarDistancia::Distancia->Auto->Auto
+alterarDistancia distancia unAuto = Auto (color unAuto) (velocidad unAuto) distancia
+---------
+{-----------------------------------------------}
