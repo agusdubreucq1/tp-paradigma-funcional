d28bb2e786eb040e5377e5b0617239052fdd007a
agusdubreucq1 <adubreucq@frba.utn.edu.ar>
2022-05-16 10:35:39 -0300

    parte 4 y cambios en: alterarDistancia y puesto

--- src/TP.hs
+++ src/TP.hs
@@ -55,8 +55,9 @@ vaGanando auto1 = (distancia auto1>).distancia
 ---------
 
 puesto::Auto->Carrera->Int
-puesto elAuto carrera = 1 + length (autos carrera) - length (filter (vaGanando elAuto) (autos carrera))
-{-puesto elAuto carrera = 1 + length (filter not.(vaGanando elAuto) (autos carrera))
+--puesto elAuto carrera = 1 + length (autos carrera) - length (filter (vaGanando elAuto) (autos carrera))
+puesto elAuto carrera = length (filter (not.(vaGanando elAuto)) (autos carrera))
+{-puesto elAuto carrera = 1 + length (filter (not.(vaGanando elAuto)) (autos carrera))
 no sirve-}
 {-SOLUCION: cantidad total de autos - cantidad de autoas a los elAuto les va ganando-}
 
@@ -65,10 +66,10 @@ no sirve-}
 {--------------------PARTE 2--------------------}
 
 correrDurante::Int->Auto->Auto
-correrDurante tiempo unAuto = Auto (color unAuto) (velocidad unAuto) (distancia unAuto + tiempo*(velocidad unAuto)) 
+correrDurante tiempo unAuto = unAuto {distancia = distancia unAuto + tiempo*(velocidad unAuto)} 
 
 alterarVelocidad::(Int->Int->Int)->Int->Auto->Auto
-alterarVelocidad funcion cantidad unAuto = Auto (color unAuto) (funcion (velocidad unAuto) cantidad) (distancia unAuto)
+alterarVelocidad funcion cantidad unAuto = unAuto {velocidad = funcion (velocidad unAuto) cantidad}
 
 bajarVelocidad::Int->Int->Int
 bajarVelocidad velocidadActual cantidad 
@@ -92,7 +93,7 @@ miguelitos cantidadABajar autoTrigger = (afectarALosQueCumplen (vaGanando autoTr
 
 jetpack::Tiempo->PowerUp
 jetpack tiempo autoTrigger carrera = afectarALosQueCumplen (==autoTrigger) 
-                                    (alterarDistancia (bonusJetpack autoTrigger (2*(velocidad autoTrigger)) tiempo))
+                                    (alterarDistancia (bonusJetpack autoTrigger (velocidad autoTrigger) tiempo))
                                     (autos carrera)
 --extra 3                                 
 type Velocidad=Int
@@ -102,6 +103,57 @@ bonusJetpack::Auto->Velocidad->Tiempo->Distancia
 bonusJetpack autoAfectado aumentoVelocidad tiempo = ((velocidad autoAfectado)+aumentoVelocidad)*tiempo
 
 alterarDistancia::Distancia->Auto->Auto
-alterarDistancia distancia unAuto = Auto (color unAuto) (velocidad unAuto) distancia
+alterarDistancia distanciaAgregada unAuto = unAuto {distancia = distancia unAuto + distanciaAgregada}
 ---------
 {-----------------------------------------------}
+
+{--------------------PARTE 4--------------------}
+type Evento = Carrera -> Carrera
+type Color = String
+
+data InfoPuesto = InfoPuesto {
+
+  posicion::Int,
+  color_info::String
+
+}deriving (Show,Eq)
+
+type TablaDePosiciones = [InfoPuesto]
+
+
+simularCarrera:: Carrera->[Evento]->TablaDePosiciones
+simularCarrera carrera = (ordenarPor posicion) . crearTabla . finalCarrera carrera
+
+------extra
+finalCarrera :: Carrera->[Evento]->Carrera
+finalCarrera carrera [] = carrera
+finalCarrera carrera lista = last lista (finalCarrera carrera (init lista))
+
+crearTabla:: Carrera -> TablaDePosiciones
+crearTabla carrera = map (infoPuestoSegunAuto carrera) (autos carrera)
+
+infoPuestoSegunAuto:: Carrera -> Auto -> InfoPuesto
+infoPuestoSegunAuto carrera auto = InfoPuesto (puesto auto carrera) (color auto)
+----------
+
+
+correnTodos:: Int -> Evento
+correnTodos tiempo = Carrera . map (correrDurante tiempo) . autos
+
+usaPowerUp:: PowerUp -> Color -> Evento
+usaPowerUp power color_auto carrera = Carrera (flip power carrera (autoSegunColor color_auto carrera))
+
+-----extra 2
+autoSegunColor:: Color -> Carrera -> Auto
+autoSegunColor color_auto = head.filter ((color_auto == ). color) . autos
+-----------
+
+-----para las pruebas
+autoA = Auto "rojo" 120 0
+autoB= Auto "blanco" 120 0
+autoC = Auto "azul" 120 0
+autoD = Auto "negro" 120 0
+carrera1 = Carrera [autoA, autoB, autoC, autoD]
+
+--simularCarrera carrera1 [correnTodos 30, usaPowerUp (jetpack 3) "azul", usaPowerUp terremoto "blanco", correnTodos 40,
+--usaPowerUp (miguelitos 20) "blanco", usaPowerUp (jetpack 6) "negro", correnTodos 10] 
